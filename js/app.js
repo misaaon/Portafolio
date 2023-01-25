20975ae2db6f7d3d58c2b299d8d651e3b88862f2
Misael Aon <misaelaon10@gmail.com>
2023-01-25 00:51:14 -0300

    Arregle responsive

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 window.addEventListener('scroll', () => {
-    let inicio = document.querySelector('.inicio');
+    const inicio = document.querySelector('.inicio');
     let ubicacion = inicio.getBoundingClientRect();
-    let nav = document.querySelector('nav');
-    let titulo = document.querySelector('.titulo');
+    const nav = document.querySelector('nav');
+    const titulo = document.querySelector('.titulo');
     
     if(ubicacion.top < 40) {
-        nav.classList.remove('hidden',);
-        titulo.classList.add('mt-10')
+        nav.classList.remove('hidden');
+        titulo.classList.add('mt-10');
     } else if(ubicacion.top > 40) {
-        nav.classList.add('hidden');
-        titulo.classList.remove('mt-10')
+        nav.classList.add('hidden', 'lg:hidden');
+        titulo.classList.remove('mt-10');
     }
-})
+});
